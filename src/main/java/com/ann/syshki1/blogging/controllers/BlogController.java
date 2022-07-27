c3416a372b31955bfd7b6fd4054c6088b1a5280c
Ann <khomiakova25@gmail.com>
2022-07-28 00:36:12 +0300

    main page

---
+++ src/main/java/com/ann/syshki1/blogging/controllers/BlogController.java
@@ -0,0 +1,15 @@
+package com.ann.syshki1.blogging.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+//    @GetMapping("/blog"){
+//        public String blogMain(Model model){
+//            model.
+//            return "blog";
+//        }
+//    }
+}
---
+++ src/main/java/com/ann/syshki1/blogging/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.ann.syshki1.blogging.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model){
+        model.addAttribute("title", "Главная строаница");
+        return "home";
+    }
+}
